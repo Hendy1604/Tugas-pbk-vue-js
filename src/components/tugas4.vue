ef72428b6a72af3d0d846aa8e1bf40c7846ea31c
Hendy Rizki <hendyrizki@student.uir.ac.id>
2025-03-16 16:26:10 +0700

    first commit

---
+++ src/components/tugas4.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+    import {ref} from 'vue';
+
+    const number = ref();
+    let result = ref();
+    let container = ref('uppCase');
+
+    
+
+    function count(){
+        result.value = eval(number.value);
+    }
+
+    function gayaFont(){
+        if(container.value == 'lowCase'){
+            container.value = 'uppCase';
+        } else {
+            container.value = 'lowcase';
+        }
+    }
+
+</script>
+
+<template>
+
+        <div v-bind:class="container">
+        <input type="text" placeholder="Masukkan angka" v-model="number">
+        <button v-on:click="count">count</button>
+        <button v-on:click="gayaFont">Ubah Gaya</button>
+
+        <br><br>
+
+        <div>Soal: <br> <b> {{ number }}</b></div><br>
+        <div>Jawaban: <br> <b> {{ result }}</b></div>
+        </div>
+
+
+
+</template>
+
+<style scoped>
+    .lowCase {
+        text-transform: lowercase;
+    }
+
+    .uppCase {
+        text-transform: uppercase;
+    }
+
+
+</style>
---
+++ src/tugas4.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import Tugas4 from "./components/tugas4.vue";
+
+createApp(Tugas4).mount("#app");
